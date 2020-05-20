c94cc67c21926093d92abfbe563e27050f6cbe8c
Jamebies <jamebieskrub@gmail.com>
2020-05-20 22:15:48 +0700

    Added tag head

---
+++ template/taghead.php
@@ -0,0 +1,15 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<title>WMinecraft</title>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+<style type="text/css">
+    @import url('https://fonts.googleapis.com/css?family=Kanit');
+    body {
+      background-color: #f5f7fa;
+      font-family: 'Kanit', sans-serif;
+      font-size: 14px;
+    }
+</style>
