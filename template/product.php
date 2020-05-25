7be89d026f80ae99a71a3e09f52967e3d337d890
phanthakan1598 <65904215+phanthakan1598@users.noreply.github.com>
2020-05-25 21:15:25 +0700

    design shop

---
+++ template/product.php
@@ -0,0 +1,91 @@
+<div class="row">
+    <div class="col-md-4 col-xs-12 mb-3">
+        <div class="card" >
+            <img src="https://www.littlekrakenboardgame.com/wp-content/uploads/2019/09/pic4975740.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <p class="card-text">
+                    <b>
+                        Detail-1<br>
+                        Detail-2
+                  </b>
+                </p>
+                <button type="button" class="btn btn-outline-success">ดูลายละเอียด</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 col-xs-12 mb-3">
+        <div class="card" >
+            <img src="https://www.littlekrakenboardgame.com/wp-content/uploads/2019/09/pic4975740.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <p class="card-text">
+                    <b>
+                        Detail-1<br>
+                        Detail-2
+                  </b>
+                </p>
+                <button type="button" class="btn btn-outline-success">ดูลายละเอียด</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 col-xs-12 mb-3">
+        <div class="card" >
+            <img src="https://www.littlekrakenboardgame.com/wp-content/uploads/2019/09/pic4975740.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <p class="card-text">
+                    <b>
+                        Detail-1<br>
+                        Detail-2
+                  </b>
+                </p>
+                <button type="button" class="btn btn-outline-success">ดูลายละเอียด</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 col-xs-12 mb-3">
+        <div class="card" >
+            <img src="https://www.littlekrakenboardgame.com/wp-content/uploads/2019/09/pic4975740.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <p class="card-text">
+                    <b>
+                        Detail-1<br>
+                        Detail-2
+                  </b>
+                </p>
+                <button type="button" class="btn btn-outline-success">ดูลายละเอียด</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 col-xs-12 mb-3">
+        <div class="card" >
+            <img src="https://www.littlekrakenboardgame.com/wp-content/uploads/2019/09/pic4975740.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <p class="card-text">
+                    <b>
+                        Detail-1<br>
+                        Detail-2
+                  </b>
+                </p>
+                <button type="button" class="btn btn-outline-success">ดูลายละเอียด</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 col-xs-12 mb-3">
+        <div class="card" >
+            <img src="https://www.littlekrakenboardgame.com/wp-content/uploads/2019/09/pic4975740.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <p class="card-text">
+                    <b>
+                        Detail-1<br>
+                        Detail-2
+                  </b>
+                </p>
+                <button type="button" class="btn btn-outline-success">ดูลายละเอียด</button>
+            </div>
+        </div>
+    </div>
+</div>
